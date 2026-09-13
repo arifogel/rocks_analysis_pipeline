f22c4e679582ccd6a50d3a6b8c880dfd9c9cae29
Ari Fogel <ari@fogelti.me>
2026-09-13 14:17:38 -0700

    add wrapper scripts

---
+++ local_ssa_katydid.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+_command() {
+  set -euxo pipefail
+  if [ "${#NOISE_PATHS[@]}" -ne 2 ]; then
+    echo "Missing NOISE_PATHS array" >&2
+    exit 1
+  fi
+  bazel run -c opt --@pypi//venv=dev //:local_ssa_katydid -- \
+    --run_name="${RUN_NAME}" \
+    --runs_base_dir="${RUNS_DIR}" \
+    --katydid_output_dir="${RUN_DIR}/root_files" \
+    --num_subruns=25 \
+    --katydid_config="${KATYDID_CONFIG}" \
+    --noise_paths "${NOISE_PATHS[@]}" \
+    --analysis_id=1 \
+    --parallelize-fields \
+    --parallelize-acquisitions
+}
+export -f _command
+
+export RUN_DIR="${RUNS_DIR}/${RUN_NAME}"
+nohup bash -c _command >& "${RUN_DIR}/local_ssa_katydid.log" &
